dd8a631d0d3da01ae4e48b67b3d3c8c5dbd413ad
Maksimas Ambrosevič <s057701@ad.viko.lt>
2024-09-12 11:46:21 +0300

    baigtas pirmas uzdavinys

--- 1 darbas/main.cpp
+++ 1 darbas/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    string vardas = "Maksim";
+    string pavarde = "Ambrosevic";
+    int amzius = 19;
+    int grupe = 1;
+    int kursas = 1;
+    string spp = "programu sistemos";
+
+    string ft = "Barcelona"; /*futbolo komanda*/
+    int ftmetai = 1899; /* ikurimo metai*/
+    
+
+    cout<<"Studento vardas - "<<vardas<<endl;
+    cout<<"Studento pavarde - "<<pavarde<<endl;
+    cout<<"Studento amzius - "<<amzius<<endl;
+    cout<<"Studento kursas - "<<kursas<<endl;
+    cout<<"Studento studiju programos pavadinimas - "<<spp;
+return 0;
 }
+
