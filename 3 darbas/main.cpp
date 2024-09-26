2272a3e3066cb59428e005b818235d5bfe71b6e2
Maksimas Ambrosevič <s057701@ad.viko.lt>
2024-09-26 14:03:20 +0300

    atlikau 4 uzduoti

--- 3 darbas/main.cpp
+++ 3 darbas/main.cpp
@@ -23,12 +23,54 @@ void funk4() {
 
 void funk5() {
     int sk;
-    int skaitmuo1;
+    int skait1;
+    int skait2;
+    int skait3;
     cout<<"Iveskite dvizenkli skaiciu:"<<endl;
     cin >> sk;
-    skaitmuo1 = sk/10;
-    cout<<skaitmuo1<<endl;
+    skait1 = sk/10;
+    skait2 = sk % 10;
+    skait3 = skait1 + skait2;
+    cout<<"Skaitmenu suma lygi: "<<skait3<<endl;
+}
 
+void funk6() {
+    int sk;
+    int skait1;
+    int skait2;
+    int skait3;
+    cout<<"Iveskite dvizenkli skaiciu:"<<endl;
+    cin >> sk;
+    skait1 = sk/10;
+    skait2 = sk % 10;
+    skait3 = skait1 - skait2;
+    cout<<"Skaitmenu skirtumas lygi: "<<skait3<<endl;
+}
+
+void funk7() {
+    int sk;
+    int skait1;
+    int skait2;
+    int skait3;
+    cout<<"Iveskite dvizenkli skaiciu:"<<endl;
+    cin >> sk;
+    skait1 = sk/10;
+    skait2 = sk % 10;
+    skait3 = skait1 * skait2;
+    cout<<"Skaitmenu sandauga lygi: "<<skait3<<endl;
+}
+
+void funk8() {
+    int sk;
+    float skait1;
+    float skait2;
+    float skait3;
+    cout<<"Iveskite dvizenkli skaiciu:"<<endl;
+    cin >> sk;
+    skait1 = sk/10;
+    skait2 = sk % 10;
+    skait3 = skait1 / skait2;
+    cout<<"Skaitmenu dalyba lygi: "<<skait3<<endl;
 }
 
 
@@ -45,8 +87,12 @@ int main(){
    }
     cout<<"---------------------------------"<<endl;
     funk5();
-
-
+    cout<<"---------------------------------"<<endl;
+    funk6();
+    cout<<"---------------------------------"<<endl;
+    funk7();
+    cout<<"---------------------------------"<<endl;
+    funk8();
 
     return 0;
 }
