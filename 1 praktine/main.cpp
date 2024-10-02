c9a9e4301fb1126d984f2bcf62ad15117c3307a7
Maksimas Ambrosevič <s057701@ad.viko.lt>
2024-10-02 14:03:17 +0300

    atlikau praktine uzduoti

--- 1 praktine/main.cpp
+++ 1 praktine/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-int pasirinkimas;
+    int pasirinkimas;
     int pasirinkimas2;
     float rez;
     float eur;
     float pirkti;
+    float parduoti;
+    float parduoti_kaina;
     float pirkti_kaina;
+
     float GBP_bendras = 0.8593;
     float GBP_pirkti = 0.8450;
     float GBP_parduoti = 0.9060;
@@ -28,47 +32,87 @@ int pasirinkimas;
     switch (pasirinkimas) {
         case 1:
             cout<<"Pasirinkite valiuta kuria norite palyginti su Eurais: "<<endl;
-            cout<<"1. Britanijos svaras"<<endl;
-            cout<<"2. Jungtiniu Amerikos valstyju doleris"<<endl;
-            cout<<"3. Indijos rupijas"<<endl;
+            cout<<"1. Britanijos svaras (GBP)"<<endl;
+            cout<<"2. Jungtiniu Amerikos valstyju doleris (USD)"<<endl;
+            cout<<"3. Indijos rupijasv (INR)"<<endl;
+            cout<<"Irasykite norimos valiutos numeri (1-3): "<<endl;
             cin>>pasirinkimas2;
         switch (pasirinkimas2) {
             case 1:
                 cout<<"Iveskite Euru kieki kuri norite palyginti su Britanijos svarais: "<<endl;
                 cin>>eur;
                 rez = eur*GBP_bendras;
-                cout<<eur<<" Eurai = "<<rez<<" Britanijos svaru"<<endl;
+                cout<<eur<<" Eurai = "<<round(rez*100)/100<<" Britanijos svaru"<<endl;
                 break;
             case 2:
                 cout<<"Iveskite Euru kieki kuri norite palyginti su Jungtiniu Amerikos valstyju doleriu: "<<endl;
                 cin>>eur;
                 rez = eur*USD_bendras;
-                cout<<eur<<" Eurai = "<<rez<<" Jungtiniu Amerikos valstyju doleriu"<<endl;
+                cout<<eur<<" Eurai = "<<round(rez*100)/100<<" Jungtiniu Amerikos valstyju doleriu"<<endl;
                 break;
             case 3:
                 cout<<"Iveskite Euru kieki kuri norite palyginti su Indijos rupijais: "<<endl;
                 cin>>eur;
                 rez = eur*INR_bendras;
-                cout<<eur<<" Eurai = "<<rez<<" Indijos rupiju"<<endl;
+                cout<<eur<<" Eurai = "<<round(rez*100)/100<<" Indijos rupiju"<<endl;
                 break;
         }
+        break;
         case 2:
             cout<<"Pasirinkite valiuta kuria norite pirkti: "<<endl;
-            cout<<"1. Britanijos svara"<<endl;
-            cout<<"2. Jungtiniu Amerikos valstyju doleri"<<endl;
-            cout<<"3. Indijos rupija"<<endl;
+            cout<<"1. Britanijos svara (GBP)"<<endl;
+            cout<<"2. Jungtiniu Amerikos valstyju doleri (USD)"<<endl;
+            cout<<"3. Indijos rupija (INR)"<<endl;
+            cout<<"Irasykite norimos valiutos numeri (1-3): "<<endl;
             cin>>pasirinkimas2;
         switch (pasirinkimas2) {
             case 1:
                 cout<<"Iveskite Britanijos svaru kieki kuri norite nupirkti: "<<endl;
                 cin>>pirkti;
                 pirkti_kaina = pirkti/GBP_pirkti;
-                cout<<pirkti<<" Britanijos svaru galite nusipirkti uz "<<pirkti_kaina<<" Euru"<<endl;
-
+                cout<<pirkti<<" Britanijos svaru galite nusipirkti uz "<<round(pirkti_kaina*100)/100<<" Euru"<<endl;
+                break;
+            case 2:
+                cout<<"Iveskite Jungtiniu Amerikos valstyju doleriu kieki kuri norite nupirkti: "<<endl;
+                cin>>pirkti;
+                pirkti_kaina = pirkti/USD_pirkti;
+                cout<<pirkti<<" Jungtiniu Amerikos valstyju doleriu galite nusipirkti uz "<<round(pirkti_kaina*100)/100<<" Euru"<<endl;
+                break;
+            case 3:
+                cout<<"Iveskite Indijos rupiju kieki kuri norite nupirkti: "<<endl;
+                cin>>pirkti;
+                pirkti_kaina = pirkti/INR_pirkti;
+                cout<<pirkti<<" Indijos rupiju galite nusipirkti uz "<<round(pirkti_kaina*100)/100<<" Euru"<<endl;
+                break;
         }
-
-
-
+        break;
+        case 3:
+            cout<<"Pasirinkite valiuta kuria norite parduoti: "<<endl;
+            cout<<"1. Britanijos svara (GBP)"<<endl;
+            cout<<"2. Jungtiniu Amerikos valstyju doleri (USD)"<<endl;
+            cout<<"3. Indijos rupija (INR)"<<endl;
+            cout<<"Irasykite norimos valiutos numeri (1-3): "<<endl;
+            cin>>pasirinkimas2;
+        switch (pasirinkimas2) {
+            case 1:
+                cout<<"Iveskite Britanijos svaru kieki kuri norite parduoti: "<<endl;
+                cin>>parduoti;
+                parduoti_kaina = parduoti/GBP_parduoti;
+                cout<<parduoti<<" Britanijos svaru galite parduoti uz "<<round(parduoti_kaina*100)/100<<" Euru"<<endl;
+                break;
+            case 2:
+                cout<<"Iveskite Jungtiniu Amerikos valstyju doleriu kieki kuri norite parduoti: "<<endl;
+                cin>>parduoti;
+                parduoti_kaina = parduoti/USD_parduoti;
+                cout<<parduoti<<" Jungtiniu Amerikos valstyju doleriu galite parduoti uz "<<round(parduoti_kaina*100)/100<<" Euru"<<endl;
+                break;
+            case 3:
+                cout<<"Iveskite Indijos rupiju kieki kuri norite parduoti: "<<endl;
+                cin>>parduoti;
+                parduoti_kaina = parduoti/INR_parduoti;
+                cout<<parduoti<<" Indijos rupiju galite parduoti uz "<<round(parduoti_kaina*100)/100<<" Euru"<<endl;
+                break;
+        }
     }
 
     return 0;
