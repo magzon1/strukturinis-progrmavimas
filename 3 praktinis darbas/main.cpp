65222b54821dc7cf4323623ca4021b5c769bb880
Maksimas Ambrosevič <s057701@ad.viko.lt>
2024-11-05 22:05:12 +0200

    sukuriau valdymo meniu

---
+++ 3 praktinis darbas/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int pasirinkimas;
+
+    while (pasirinkimas !=3){
+    cout<<endl;
+    cout<<"Pasirinkite kokiu budu norite sifruoti/desifruoti teksta (iveskite skaiciu nuo 1 iki 3):"<<endl;
+    cout<<"   1. Naudojant abecele"<<endl;
+    cout<<"   2. Naudojant ASCII koduote"<<endl;
+    cout<<endl;
+    cout<<"   3. Uzdaryti meniu"<<endl;
+    cin>>pasirinkimas;
+
+        switch (pasirinkimas){
+            case 1:
+                break;
+
+            case 2:
+                break;
+        }
+    }
+
+
+    return 0;
+}
