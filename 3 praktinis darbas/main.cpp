a7d7a5015cee4dee6f7fa389da5ab275e4504efd
Maksimas Ambrosevič <s057701@ad.viko.lt>
2024-11-21 18:37:52 +0200

    sukuriau funkcija su ASCII koduote (dar reikia patobulinti)

--- 3 praktinis darbas/main.cpp
+++ 3 praktinis darbas/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 using namespace std;
 
+
+//Sifravimas/Desifravimas naudojant abecele:
+
 const char abecele[] = "abcdefghijklmnopqrstuvwxyz";
 const int abecele_SIZE = sizeof(abecele) - 1;
 
@@ -23,7 +26,7 @@ string normalizeText(const string& text) {
     return normalized;
 }
 
-string vigenereEncrypt(const string& text, const string& key) {
+string encryptedAbc(const string& text, const string& key) {
     string normalizedText = normalizeText(text);
     string normalizedKey = normalizeText(key);
     string encryptedText;
@@ -41,7 +44,7 @@ string vigenereEncrypt(const string& text, const string& key) {
     return encryptedText;
 }
 
-string vigenereDecrypt(const string& encryptedText, const string& key) {
+string decryptedAbc(const string& encryptedText, const string& key) {
     string normalizedKey = normalizeText(key);
     string decryptedText;
 
@@ -58,46 +61,116 @@ string vigenereDecrypt(const string& encryptedText, const string& key) {
     return decryptedText;
 }
 
+//Sifravimas/Desifravimas naudojant ASCII koduote:
+
+string encryptedAscii(const string& text, const string& key) {
+    string encryptedText;
+    int keyLength = key.length();
+    for (size_t i = 0; i < text.length(); i++) {
+        char encryptedChar = (text[i] + key[i % keyLength]) % 256; // ASCII rėžyje
+        encryptedText += encryptedChar;
+    }
+    return encryptedText;
+}
+
+string decryptedAscii(const string& encryptedText, const string& key) {
+    string decryptedText;
+    int keyLength = key.length();
+    for (size_t i = 0; i < encryptedText.length(); i++) {
+        char decryptedChar = (encryptedText[i] - key[i % keyLength] + 256) % 256;
+        decryptedText += decryptedChar;
+    }
+    return decryptedText;
+}
+
+
+
 
 int main() {
-    int pasirinkimas;
-    string text, key, option;
+    int choise, choise2;
+    string text, key, result;
 
-    while (pasirinkimas != 3){
+    while (choise != 3) {
         cout<<endl;
         cout<<"Pasirinkite kokiu budu norite sifruoti/desifruoti teksta (iveskite skaiciu nuo 1 iki 3):"<<endl;
         cout<<"   1. Naudojant abecele"<<endl;
         cout<<"   2. Naudojant ASCII koduote"<<endl;
         cout<<endl;
         cout<<"   3. Uzdaryti meniu"<<endl;
-        cin>>pasirinkimas;
+        cin>>choise;
 
-        if (pasirinkimas == 1) {
-            cout << "Iveskite teksta kuri nori uzsifruoti: "<<endl;
-            cin >> text;
-            cout << "Iveskite slapta rakta "<<endl;
-            cin >> key;
-            string encryptedText = vigenereEncrypt(text, key);
-            cout << "Uzsifruotas tekstas: " << encryptedText << endl;
+        if (choise == 1) {
+            cout<<"Koki veiksma norite atlikti?"<<endl;
+            cout<<"1. Sifruoti teksta"<<endl;
+            cout<<"2. Desifruoti teksta"<<endl;
+            cout<<endl;
+            cout<<"3. Grizti atgal"<<endl;
+            cin>>choise2;
+
+            if (choise2 == 1) {
+                cout<<"Iveskite teksta kuri nori uzsifruoti: "<<endl;
+                cin>>text;
+                cout<<"Iveskite slapta rakta "<<endl;
+                cin>>key;
+                string encryptedText = encryptedAbc(text, key);
+                cout<<"Uzsifruotas tekstas: "<<encryptedText<<endl;
+            }
+
+            else if (choise2 == 2) {
+                cout<<"Iveskite teksta kuri norite desifruoti: "<<endl;
+                cin>>text;
+                cout<<"Iveskite slapta rakta: "<<endl;
+                cin >> key;
+                string decryptedText = decryptedAbc(text, key);
+                cout<<"Desifruotas tekstas: "<<decryptedText<<endl;
+            }
+            else if (choise2 == 3) {
+                return main();
+            }
+            else {
+                cout<<"Tokio pasirinkimo nera!"<<endl;
+            }
         }
 
-        else if (pasirinkimas == 2) {
-            cout << "Iveskite teksta kuri norite desifruoti: "<<endl;
-            cin >> text;
-            cout << "Iveskite slapta rakta: "<<endl;
-            cin >> key;
-            string decryptedText = vigenereDecrypt(text, key);
-            cout << "Desifruotas tekstas: " << decryptedText << endl;
+        else if (choise == 2) {
+            cout<<"Koki veiksma norite atlikti?"<<endl;
+            cout<<"1. Sifruoti teksta"<<endl;
+            cout<<"2. Desifruoti teksta"<<endl;
+            cout<<endl;
+            cout<<"3. Grizti argal"<<endl;
+            cin>>choise2;
+
+            if (choise2 == 1) {
+                cout<<"Iveskite teksta kuri nori uzsifruoti: "<<endl;
+                cin>>text;
+                cout<<"Iveskite slapta rakta "<<endl;
+                cin>>key;
+                result = encryptedAscii(text, key);
+                cout << "Uzsifruotas tekstas: " << result << endl;
+            }
+
+            else if (choise2 == 2) {
+                cout<<"Iveskite teksta kuri nori desifruoti: "<<endl;
+                cin>>text;
+                cout<<"Iveskite slapta rakta "<<endl;
+                cin>>key;
+                result = decryptedAscii(text, key);
+                cout << "Desifruotas tekstas: "<<result<<endl;
+            }
+            else if (choise2 == 3) {
+                return main();
+            }
+            else {
+                cout<<"Tokio pasirinkimo nera!"<<endl;
+            }
         }
-        else if (pasirinkimas == 3) {
+        else if (choise == 3) {
             return 0;
         }
-
         else {
-            cout<<"Tokio pasirinkimo nera"<<endl;
+            cout<<"Tokio pasirinkimo nera!"<<endl;
         }
     }
-
     return 0;
 }
 
